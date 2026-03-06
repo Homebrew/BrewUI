13ef0fbbcca9d80f7e0650869b94d69de98f89b8
Graeme Arthur <garthur@duckduckgo.com>
2026-03-06 18:30:10 +1100

    Create Xcode project

---
+++ BrewUI/BrewUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  BrewUIApp.swift
+//  BrewUI
+//
+//  Created by Graeme Arthur on 6/3/2026.
+//
+
+import SwiftUI
+
+@main
+struct BrewUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BrewUITests/BrewUITests.swift
@@ -0,0 +1,17 @@
+//
+//  BrewUITests.swift
+//  BrewUITests
+//
+//  Created by Graeme Arthur on 6/3/2026.
+//
+
+import Testing
+@testable import BrewUI
+
+struct BrewUITests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
