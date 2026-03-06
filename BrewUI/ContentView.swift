fd0fe4d151cec18fa167315f0ebb1e225c068808
Graeme Arthur <garthur@duckduckgo.com>
2026-03-06 18:30:10 +1100

    Create Xcode project

---
+++ BrewUI/ContentView.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  BrewUI
+//
+//  Created by Graeme Arthur on 6/3/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Text("🍺").font(.largeTitle)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
